5dd2d4fc426bf417fd979d6a3941116a5dc00e4a
Carson.Ho <carson.ho@foxmail.com>
2017-09-28 16:42:05 +0800

    add

--- app/src/main/java/scut/carson_ho/androidlearningmaterial/MainActivity.java
+++ app/src/main/java/scut/carson_ho/androidlearningmaterial/MainActivity.java
@@ -5,12 +5,13 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    int a = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        
+    a = 3;
 
     }
 }
